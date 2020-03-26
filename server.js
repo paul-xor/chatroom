b4f9c6067b41a5833a5668c6c66d9aaac8e6aa35
Pasha <palexsmol@gmail.com>
2020-03-25 18:48:09 -0700

    add socket messages to console: joined, disconnect etc

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 //Run when client connects
 io.on("connection", socket => {
   console.log("New WS Connection ...");
+
+  //Welcome current user.
+  socket.emit("message", "Welcome to ChatCord!");
+
+  // BroadCast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat.");
+
+  //Runs when clien disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat.");
+  });
 });
 
 const PORT = 7777 || process.env.PORT;
