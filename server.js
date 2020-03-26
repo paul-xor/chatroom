12f6a3a244b267b63838873e69b780ff1089507b
Pasha <palexsmol@gmail.com>
2020-03-25 18:24:58 -0700

    add socket

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+//Run when client connects
+io.on("connection", socket => {
+  console.log("New WS Connection ...");
+});
 
 const PORT = 7777 || process.env.PORT;
 
