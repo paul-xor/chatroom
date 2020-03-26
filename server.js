5743734988a46b86a99ce04416ea85223331173d
Pasha <palexsmol@gmail.com>
2020-03-26 12:01:22 -0700

    add formatMessage

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+const botName = "ChatCord Bot";
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -14,19 +17,22 @@ io.on("connection", socket => {
   console.log("New WS Connection ...");
 
   //Welcome current user.
-  socket.emit("message", "Welcome to ChatCord!");
+  socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
   // BroadCast when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat.");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat.")
+  );
 
   //Runs when clien disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat.");
+    io.emit("message", formatMessage(botName, "A user has left the chat."));
   });
 
   //Listen for chat message
   socket.on("chatMessage", msg => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
